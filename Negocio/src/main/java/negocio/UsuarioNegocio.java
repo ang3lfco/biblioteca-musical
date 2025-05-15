b835ca788f76e1f6891bdd9949ef79ff3bbf8ca3
ang3lfco <ang3lfco@outlook.com>
2025-05-15 03:09:34 -0700

    Creacion de Usuario clases persistencia, negocio & dto

---
+++ Negocio/src/main/java/negocio/UsuarioNegocio.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package negocio;
+
+import interfaces.IUsuarioNegocio;
+
+/**
+ *
+ * @author ang3lfco
+ */
+public class UsuarioNegocio implements IUsuarioNegocio {
+    
+}
