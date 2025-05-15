b4fb6efbb802e9283ceacc9d35fa667ce2ae170a
ang3lfco <ang3lfco@outlook.com>
2025-05-14 22:42:18 -0700

    Capas de proyecto

---
+++ Modelos/src/main/java/com/mycompany/modelos/Modelos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modelos;
+
+/**
+ *
+ * @author ang3lfco
+ */
+public class Modelos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
