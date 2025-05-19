7524a6f7b01d10f717a6f6f691b3ec16ea8f8b78
Oribiel <oribiel14@gmail.com>
2025-05-18 22:49:21 -0600

    Conexion con interfaces en la clase InsercionMasvia
    agregacion de consttructor y atributos

--- Presentacion/src/main/java/datos/InsercionMasiva.java
+++ Presentacion/src/main/java/datos/InsercionMasiva.java
@@ -4,11 +4,42 @@
  */
 package datos;
 
+import daos.AlbumDAO;
+import daos.ArtistaDAO;
+import daos.CancionDAO;
+import daos.GeneroDAO;
+import daos.PersonaDAO;
+import interfaces.IAlbumNegocio;
+import interfaces.IArtistaNegocio;
+import interfaces.ICancionNegocio;
+import interfaces.IGeneroNegocio;
+import interfaces.IPersonaNegocio;
+import negocio.AlbumNegocio;
+import negocio.ArtistaNegocio;
+import negocio.CancionNegocio;
+import negocio.GeneroNegocio;
+import negocio.PersonaNegocio;
+
 /**
  *
  * @author ang3lfco
  */
 public class InsercionMasiva {
-    public void iniciarInsercionMasiva(){
+
+    private final IPersonaNegocio personaNegocio;
+    private final IGeneroNegocio generoNegocio;
+    private final IArtistaNegocio artistaNegocio;
+    private final IAlbumNegocio albumNegocio;
+    private final ICancionNegocio cancionNegocio;
+
+    public InsercionMasiva() {
+        this.personaNegocio = new PersonaNegocio(new PersonaDAO());
+        this.generoNegocio = new GeneroNegocio(new GeneroDAO());
+        this.artistaNegocio = new ArtistaNegocio(new ArtistaDAO());
+        this.albumNegocio = new AlbumNegocio(new AlbumDAO());
+        this.cancionNegocio = new CancionNegocio(new CancionDAO());
+    }
+
+    public void insertaDatos() {
     }
 }
